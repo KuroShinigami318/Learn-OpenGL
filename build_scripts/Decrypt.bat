507e9565be206d1ae89f267d9ad5e855d97d70f0
bao.duongngoc <duongngocbao318@gmail.com>
2024-02-01 01:14:08 +0700

    added bootstrap.py and readme

--- build_scripts/Decrypt.bat
+++
@@ -1,9 +0,0 @@
-@echo off &setlocal
-rem https://www.dostips.com/forum/viewtopic.php?t=9420
-set encrypted=%1
-for /f "delims=" %%i in (
- 'powershell -nop -ep bypass -c "[System.Net.NetworkCredential]::new('', (ConvertTo-SecureString -String '%encrypted%')).Password"'
-) do set decrypted=%%i
-
-
-endlocal & set %~2=%decrypted%
